4c64ec192be402a5752d9f8cf00e26fbb92c75e2
Jamie Jones <jamie.mar.jones14@gmail.com>
2021-05-20 18:36:32 -0400

    some css work as well as a carousel added using bootstrap. get featured button links to google form

--- flipdown.js
+++
@@ -1,6 +0,0 @@
-// we will have a flipdown annimation for the countdown
-// because that will look dope as hell
-
-// how to do that: https://github.com/PButcher/flipdown#flipdown
-// Different kinds!:
-//  https://1stwebdesigner.com/9-free-open-source-flipping-clocks-using-css-javascript/
